d73aeb41a993a8595cf9066d169a0e06e410691e
DeivisonSantos <deivisonddj3@gmail.com>
2024-05-28 12:46:05 -0300

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,30 @@
+import React from "react"
+import "./styles.css"
+import Link from "next/link"
+
 const LoginPage = () => {
+
     return(
-        <h1>Login Page</h1>
+        <section id="login-section">
+            <form>
+                {/* <label htmlFor="name-input-field">Name</label>
+                <input type="text" id="name-input-field"/> */}
+                
+                <input placeholder="Email*" type="email" id="email-input-field"/>
+                <span className="active-field-underline"></span>
+
+                <input placeholder="Password*" type="password" id="password-input-field"/>
+                <span className="active-field-underline"></span>
+
+                <div className="buttons-div">
+                    <Link href="/" className="return-button">Return</Link>
+
+                    <div className="submit-button-container">
+                        <button type="submit" className="submit-button">LogIn</button>
+                    </div>
+                </div>
+            </form>
+        </section>
     )
 }  
 
