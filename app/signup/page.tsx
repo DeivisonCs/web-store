3271032e38383ae255882dd23f57b5c4af81bdf4
DeivisonSantos <deivisonddj3@gmail.com>
2024-06-16 10:41:02 -0300

    input field component and dinamic header

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,30 +1,37 @@
 import React from "react"
+import Header from "../components/Header"
+import InputField from "../components/InputField"
 import "./styles.css"
 import Link from "next/link"
 
 const SignUp = () => {
 
     return(
-        <section id="login-section">
+        <>
+        <Header logo={true} searchBar={false} products={false} account={false}/>
+
+        <section id="signup-section">
             <form>
-                <input placeholder="Name*" type="text" id="name-input-field"/>
-                <span className="active-field-underline"></span>
+                <InputField label="Name" type="text" isRequired={true}/>
                 
-                <input placeholder="Email*" type="email" id="email-input-field"/>
-                <span className="active-field-underline"></span>
+                <InputField label="Email" type="email" isRequired={true}/>
 
-                <input placeholder="Password*" type="password" id="password-input-field"/>
-                <span className="active-field-underline"></span>
+                <InputField label="Password" type="password" isRequired={true}/>
 
-                <div className="buttons-div">
+                <div className="signup-buttons-div">
                     <Link href="/" className="return-button">Return</Link>
 
                     <div className="submit-button-container">
                         <button type="submit" className="submit-button">Create Account</button>
                     </div>
                 </div>
+
+                <div className="assistant-access-link">
+                    <Link href="/login">Already have an account?</Link>
+                </div>
             </form>
         </section>
+        </>
     )
 }  
 
