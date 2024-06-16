b79b5102b24ae0efc4f8fc0b377f5fe07164c963
DeivisonSantos <deivisonddj3@gmail.com>
2024-06-16 10:39:20 -0300

    added dinamic to header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -3,18 +3,31 @@ import Link from "next/link"
 import SearchBar from "../SearchBar"
 import "./styles.css"
 
-const Header = () => {
+interface HeaderProps {
+    logo: boolean
+    searchBar: boolean
+    products: boolean
+    account: boolean
+}
+
+const Header: React.FC<HeaderProps> = ({logo, searchBar, products, account}) => {
     return (
         <section id="header-section">
+            {logo &&
             <span className="logo-span">
-                <img src="/WebStoreLogo.png"/>
-            </span>
+                <Link href="/"><img src="/WebStoreLogo.png"/></Link>
+            </span>}
 
-            <SearchBar/>
+            {searchBar && <SearchBar/>}
 
-            <div>
-                {/* Deve aparecer apenas se o usuário não estiver logado  */}
-                <Link href="/login">Register/LogIn</Link>
+            <div className="interaction-links profile">
+                <nav className="navegation">
+                    {products && <Link href="/products" id="link-to-products">Products</Link>}
+
+                    {/* Deve aparecer apenas se o usuário não estiver logado  */}
+                    { account && <Link href="/login">Register/LogIn</Link>}
+                </nav>
+
                 <img />
             </div>
         </section>
