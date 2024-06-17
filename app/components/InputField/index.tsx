5e85ec20dd04ffe64dc0fab41fb2f9bfdf804419
DeivisonSantos <deivisonddj3@gmail.com>
2024-06-17 10:16:59 -0300

    show/hidde password

--- app/components/InputField/index.tsx
+++ app/components/InputField/index.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+"use client"
+
+import React, { FC, useState } from "react";
 import "./styles.css"
 
 interface InputFieldProps {
@@ -8,13 +10,27 @@ interface InputFieldProps {
 }
 
 const InputField: React.FC<InputFieldProps> = ({label, type, isRequired}) => {
+    const hideIcon = '/icons/hidePasswordIcon.svg'
+    const showIcon = '/icons/showPasswordIcon.svg'
+
+    const [passVisibility, setVisibility] = useState(type);
+    const [passwordIcon, setPasswordIcon] = useState(showIcon)
+
     const placeHolder = isRequired?`${label}*`: label
 
+    const changeVisibility = () => {
+        setVisibility((prev) => prev==='password'? 'text':'password');
+        setPasswordIcon((prev) => prev===showIcon? hideIcon: showIcon);
+    }
+
+
     return (
-        <>
-            <input placeholder={placeHolder} type={type} className="input-field-component"/>
+        <div className="input-field-component">
+            <input placeholder={placeHolder} type={passVisibility} className="input-field"/>
             <span className="active-field-underline"></span>
-        </>
+            
+            {type==="password" && <span className="change-visibility" onClick={changeVisibility}><img src={passwordIcon} className="visibility-icon"/></span>}
+        </div>
     )
 }
 
